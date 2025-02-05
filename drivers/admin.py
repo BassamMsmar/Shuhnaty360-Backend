db4d2796f5c74cb53b2553ed5d33c2a7671c9cf5
Bassam msmar <bassammsmar@gmail.com>
2025-02-05 12:04:56 +0300

    Register Driver model with admin interface

--- drivers/admin.py
+++ drivers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Driver
 # Register your models here.
+
+
+
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('name', 'identity_number', 'nationality', 'vehicle_number', 'status')
+    search_fields = ('name', 'identity_number', 'vehicle_number')
+
+    
+admin.site.register(Driver, DriverAdmin)
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'drivers',
+    'customers',
+    'shipments',
+    'recipient',
 ]
 
 MIDDLEWARE = [
