3d77d0ccb866f9b9d52ac11e7540fcf77a5a67c1
Bassammsmar <bassammsmar@gmail.com>
2025-03-12 20:46:51 +0300

    edit info db

--- project/settings.py
+++ project/settings.py
@@ -101,11 +101,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='127.0.0.1'),
-        'PORT': config('DB_PORT', default='5432'),
+        'NAME': 'aljeed_db',
+        'USER': 'admin',
+        'PASSWORD': 'aljeed22',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
