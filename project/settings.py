42c5f955566796ec86572ec6b0af6d15c871cea5
bassammsmar <bassammsmar@gmail.com>
2025-03-15 12:02:15 +0000

    edit info db

--- project/settings.py
+++ project/settings.py
@@ -79,8 +79,7 @@ TEMPLATES = [
             ],
         },
     },
-]
-
+}
 WSGI_APPLICATION = 'project.wsgi.application'
 
 REST_FRAMEWORK = {
@@ -91,24 +90,22 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aljeed_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'aljeed_db',
-#         'USER': 'admin',
-#         'PASSWORD': 'aljeed22',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
