7653fa4f4de3e2f99e6f22fd4a6d7cefdf9e9900
BassamMsmar <Bassammsmar@gmail.com>
2025-03-15 15:13:01 +0300

    fix template code

--- project/settings.py
+++ project/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
             ],
         },
     },
-}
+]
 WSGI_APPLICATION = 'project.wsgi.application'
 
 REST_FRAMEWORK = {
@@ -90,12 +90,13 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
