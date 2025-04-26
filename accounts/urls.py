5ba917a6398df0225f2014b093532caa677bd061
BassamMsmar <Bassammsmar@gmail.com>
2025-04-26 15:05:15 +0300

    add detail user to accountd app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UsersViewSet
+from .views import UsersViewSet,UserDetaliCreateSet
 
 urlpatterns = [
     path('api/', UsersViewSet.as_view(), name='user-list-create'),
+    path('api/<int:pk>/', UserDetaliCreateSet.as_view(), name='user-detail-update-delete'),
+    
 ]
