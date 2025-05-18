d1554ae7f7eaaf65951c6ad3241da9270faceed4
bassammsmar <bassammsmar@gmail.com>
2025-05-18 20:39:45 +0300

    edit name serializers to DriverRetrieveUpdateDestroy in driver app '

--- drivers/urls.py
+++ drivers/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import DriverViewSet, DriverRetrieveUpdateDestroyt, TruckTypeViewSet
+from .views import DriverViewSet, DriverRetrieveUpdateDestroy, TruckTypeViewSet
 
 urlpatterns = [
     path('api/', DriverViewSet.as_view(), name='Driver-list-create'),
     path('api/TruckType/', TruckTypeViewSet.as_view(), name='TruckType-list-create'),
-    path('api/<int:pk>', DriverRetrieveUpdateDestroyt.as_view(), name='Driver-detail'),
+    path('api/<int:pk>', DriverRetrieveUpdateDestroy.as_view(), name='Driver-detail'),
 ]
