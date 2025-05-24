e532ae074e90c70fb2c0c37fd7b8048112388828
BassamMsmar <Bassammsmar@gmail.com>
2025-05-24 14:48:50 +0300

    feat: enhance client and company branch serialization and API endpoints
    - Add CompanyBranchSerializer to accounts app
    - Refactor client serializers for better organization
    - Update client API endpoints and views
    - Add new client branch create endpoint
    - Update profile_company serializers with ref_name
    - Add new dependencies: psycopg2-binary and rest-framework-simplejwt

--- clients/urls.py
+++ clients/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import ClientViewSet, ClientDetail, ClientBranchList, ClientBranchCreate, ClientBranchSDetail
 
 urlpatterns = [
-    path('api/', ClientViewSet.as_view(), name='client-list-create'),
-    path('api/<int:pk>', ClientDetail.as_view(), name='client-details-create'),
-    path('api/branch/', ClientBranchList.as_view(), name='client-branch-details-create'),
-    path('api/branch/<int:pk>', ClientBranchSDetail.as_view(), name='client-branch-details-create'),
+    path('', ClientViewSet.as_view(), name='client-list-create'),
+    path('<int:pk>', ClientDetail.as_view(), name='client-details-create'),
+    path('branch/', ClientBranchList.as_view(), name='client-branch-details-create'),
+    path('branch/<int:pk>', ClientBranchSDetail.as_view(), name='client-branch-details-create'),
+    path('branch/create', ClientBranchCreate.as_view(), name='client-branch-create'),
 ]
--- profile_company/serializers.py
+++ profile_company/serializers.py
@@ -11,6 +11,7 @@ class CompanyProfileSerializer(serializers.ModelSerializer):
 class CompanyBranchSerializer(serializers.ModelSerializer): 
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+    ref_name = "ProfileCompanyBranch"
     class Meta:
         model = CompanyBranch
         fields = '__all__'  
