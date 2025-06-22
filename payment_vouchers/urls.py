19d5a4d5cacb1696d991ae6687479f65ea66cf2d
bassammsmar <bassammsmar@gmail.com>
2025-06-23 01:19:26 +0300

    add crud to payment

--- payment_vouchers/urls.py
+++ payment_vouchers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PaymentVoucherListView, PaymentVoucherDetailView , PaymentVoucherCreateView, PaymentVoucherUpdateView
+from .views import PaymentVoucherListView, PaymentVoucherDetailView , PaymentVoucherCreateView, PaymentVoucherUpdateView, PaymentVoucherOptionsView
 
 urlpatterns = [
     path('', PaymentVoucherListView.as_view(), name='payment-voucher-list'),
     path('create/', PaymentVoucherCreateView.as_view(), name='payment-voucher-create'),
     path('<int:pk>/update', PaymentVoucherUpdateView.as_view(), name='payment-voucher-update'),
     path('<int:pk>/', PaymentVoucherDetailView.as_view(), name='payment-voucher-detail'),
+    path('options/', PaymentVoucherOptionsView.as_view(), name='payment-voucher-options'),
 ]
