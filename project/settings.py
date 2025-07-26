56c14a5ee13826c7ad659d13988ca63476bb4175
bassammsmar <bassammsmar@gmail.com>
2025-07-26 19:52:46 +0300

    edit : settings to allow hosteing

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 # Frontend settings
 FRONTEND_URL = os.getenv('FRONTEND_URL')
